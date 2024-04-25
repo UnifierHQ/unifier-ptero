6f9325b7ef811ace1ebb6b350d2dafb97a584ee5
green <41323182+greeeen-dev@users.noreply.github.com>
2024-04-25 16:08:31 +0200

    Delete template_check.py

--- template_check.py
+++
@@ -1,8 +0,0 @@
-async def check(bot):
-    """If you need certain code to be ran before unload/reload,
-    write them here.
-
-    shutdown must be set to true and utils must include
-    [plugin_id]_check.py."""
-
-    return
